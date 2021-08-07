850121a3f6d49cae2405bdee940df70b597439fd
devilbas07 <devilbastopoftheworld@gmail.com>
2021-08-07 21:30:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tddpn7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tddpn7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  balance: string;
+  needPercnet: string = '50';
+  wantPercnet: string = '30';
+  investPercnet: string = '20';
+  calculateText: string;
+
+  calculatePercent = (a: number, b: number) => a * (b / 100);
+  calculateBalance() {
+    if (
+      this.balance &&
+      this.needPercnet &&
+      this.wantPercnet &&
+      this.investPercnet &&
+      (isNaN(+this.balance) ||
+        isNaN(+this.needPercnet) ||
+        isNaN(+this.wantPercnet) ||
+        isNaN(+this.investPercnet))
+    ) {
+      this.calculateText = '';
+      return;
+    }
+
+    const need = this.calculatePercent(+this.balance, +this.needPercnet);
+    const want = this.calculatePercent(+this.balance, +this.wantPercnet);
+    const invest = this.calculatePercent(+this.balance, +this.investPercnet);
+
+    this.calculateText = `
+เงินเดือน ${this.balance} บาท
+เงินใช้จ่าย ${need} บาท
+เงินซื้อของที่อยากได้ ${want} บาท
+เงินออม / ลงทุน ${invest} บาท
+    `;
+  }
+}
